82a64ef7738891f99f15d8fb780cb1260d5e425d
Sebastian Roth <sebastian@nextbase.co.uk>
2020-03-30 12:22:00 +0100

    Integrate a few fixes from upstream

--- lib/flutter_masked_text.dart
+++ lib/flutter_masked_text.dart
@@ -60,8 +60,13 @@ class MaskedTextController extends TextEditingController {
   @override
   void set text(String newText) {
     if (super.text != newText) {
-      super.text = newText;
-      this.moveCursorToEnd();
+      int position = this.selection.baseOffset >= super.text.length
+           ? (newText ?? '').length
+           : this.selection.baseOffset;
+       value = value.copyWith(
+           text: newText,
+           selection: TextSelection.collapsed(offset: position),
+           composing: TextRange.empty);
     }
   }
 
